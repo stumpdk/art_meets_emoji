738e43927f002ad89cba34ccbf0cdd76c1b400b7
StumpDK <stumpdenmark@hotmail.com>
2017-09-30 17:09:53 +0200

    Added db pooling

--- src/models/db.js
+++ src/models/db.js
@@ -1,22 +1,23 @@
 var mysql = require('mysql');
 
+var pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'example.org',
+  user            : 'bob',
+  password        : 'secret',
+  database        : 'my_db'
+});
+
 module.export = {
+    subscribeUser = function(userId){
+        pool.query('insert into subscribtions (user, subscribtion_date) VALUES ()', );
+    },
     searchImagesByText = function(userId, text, cb){
-        var connection = mysql.createConnection({
-          host     : 'localhost',
-          user     : 'me',
-          password : 'secret',
-          database : 'my_db'
-        });
 
-        connection.connect();
-
-        connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
           if (error) throw error;
-          console.log('The solution is: ', results[0].solution);
+          console.log('The solution is: ', results[0]);
           cb(results);
         });
-
-        connection.end();
     }
 };
