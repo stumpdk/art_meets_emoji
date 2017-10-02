2ea3ba99b66905e14ff070d806f3d1451ead580f
Bo Henriksen <stumpdenmark@hotmail.com>
2017-10-02 15:02:11 +0200

    Moved gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,7 +39,7 @@ module.exports = function(grunt) {
 
         },
         "jsbeautifier": {
-            files: ["**/*.js", "!node_modules/**/*.js"],
+            files: ["./src/**/*.js"],
             options: {}
         }
     });
