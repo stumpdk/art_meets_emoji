76b6c8b2d8472aaf9e0d561ac4be43657297d8bd
StumpDK <stumpdenmark@hotmail.com>
2018-02-25 14:17:59 +0100

    Added importer and new table

--- csv_to_mysql.py
+++ csv_to_mysql.py
@@ -12,20 +12,19 @@ mydb = pymysql.connect(host='artmeetsemoji-cluster.cluster-cf2nstf005qs.eu-west-
     db='art_meets_emoji',
     charset='utf8')
 cursor = mydb.cursor()
-mydb.query('delete from new_import');
+mydb.query('delete from new_import2');
 #restval='',delimiter=',',doublequote=False,strict=True,
-reader = csv.DictReader(open('SMK_changed.csv','r',encoding='utf8'),doublequote=True,skipinitialspace=True)
+reader = csv.DictReader(open('SMK.csv','r',encoding='utf8'),doublequote=True,skipinitialspace=True)
+
+startQuery = '''INSERT INTO new_import2 (`medium_image_url`, `id`, `artist_auth`, `artist_name`, `artist_name_text`, `artist_natio_dk`, `category`, `comments`, `content_notes`,
+`description_note_dk`, `materiale_type`, `materiale_type_eng`, `multi_work_ref`, `note_elementer`, `proveniens`, `related_id`, `title_all`, `title_dk`, `title_eng`,`creation_dk`) VALUES '''
+
+insertQuery = ''
+
 i = 0
 for row in reader:
     #print(row)
-    query = '''
-      INSERT INTO new_import(
-      `medium_image_url`, `id`, `artist_auth`, `artist_name`, `artist_name_text`, `artist_natio_dk`, `category`, `comments`, `content_notes`,
-      `description_note_dk`, `materiale_type`, `materiale_type_eng`, `multi_work_ref`, `note_elementer`, `proveniens`, `related_id`, `title_all`, `title_dk`, `title_eng`)
-      VALUES(
-        "%s", "%s", "%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"
-        )
-        ''' % (
+    insertQuery = insertQuery + '''("%s", "%s", "%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s"),''' % (
           convertString(row, 'medium_image_url'),
           convertString(row, 'id'),
           convertString(row, 'artist_auth'),
@@ -44,23 +43,27 @@ for row in reader:
           convertString(row, 'related_id'),
           convertString(row, 'title_all'),
           convertString(row, 'title_dk'),
-          convertString(row, 'title_eng')
+          convertString(row, 'title_eng'),
+          convertString(row, 'object_production_date_text_dk')
          )
     #print(query)
-    try:
-        cursor.execute(query)
-    except Exception as e:
-        print(e);
+
     i = i+1
-    if i%1000 == 0:
+    if i%250 == 0:
         print('committing')
+        #print(query)
+        #sys.exit(0)
         try:
-            mydb.commit()
+            cursor.execute(startQuery + insertQuery[:-1])
+            mydb.commit();
         except Exception as e:
-            print(e)
+            print(e);
 
+        insertQuery = ''
+
+cursor.execute(startQuery + insertQuery[:-1])
 mydb.commit()
-
+print("added %d rows" % i)
 #close the connection to the database.
 cursor.close()
 print ("Done")
